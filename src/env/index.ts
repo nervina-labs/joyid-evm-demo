f3451afd1b98d211b7bf6387a12ebb1a1fc1a02a
yuche <i@yuche.me>
2024-09-09 12:34:45 +0800

    refactor: update

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,8 @@ export const SEPOLIA_RPC_URL =
   'https://ethereum-sepolia.blockpi.network/v1/rpc/public'
 
 export const JOY_ID_URL =
-  import.meta.env.VITE_APP_JOY_ID_URL ?? 'https://app.joy.id'
+  import.meta.env.VITE_APP_JOY_ID_URL ??
+  'https://joyid-app-git-fix-pwa2-nervina.vercel.app'
 
 export const EXPLORER_URL =
   import.meta.env.VITE_APP_EXPLORER_URL ?? 'https://sepolia.etherscan.io'
